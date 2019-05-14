8891b4ea0762f2d1b7e0f803e40933ff153ca72c
maugares <mauro.garciaesteban@gmail.com>
2019-05-14 21:05:24 +0200

    externalise calculation of emission other

---
+++ src/formulas/emissionOther.js
@@ -0,0 +1,19 @@
+export const emissionOther = (weightList) => {
+    const industryEmission = weightList.map((industries, index, array) => {
+        if (index < array.length) {
+            return industries.kgPerEuro
+        }
+    })
+
+    const emissionOther = industryEmission.reduce(
+        (total, emissions, index, array) => {
+            total += emissions
+            if (index === (array.length - 1)) {
+                return total / (array.length)
+            } else {
+                return total
+            }
+        })
+
+    return emissionOther
+}
