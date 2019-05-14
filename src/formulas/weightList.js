40f58dcbb887538bbacd8f6af0e753c6b9ec3454
maugares <mauro.garciaesteban@gmail.com>
2019-05-14 17:41:42 +0200

    add tonPerEuro to weightList

--- src/formulas/weightList.js
+++ src/formulas/weightList.js
@@ -5,61 +5,71 @@ const weightList = [
         industry: industry[0],
         weightS1: 0.50,
         weightS2: 0.05,
-        weightS3: 0.45
+        weightS3: 0.45,
+        tonPerEuro: 1.763
     },
     {
         industry: industry[1],
         weightS1: 0.20,
         weightS2: 0.10,
-        weightS3: 0.70
+        weightS3: 0.70,
+        tonPerEuro: 1.369
     },
     {
         industry: industry[2],
         weightS1: 0.20,
         weightS2: 0.10,
-        weightS3: 0.70
+        weightS3: 0.70,
+        tonPerEuro: 0.239
     },
     {
         industry: industry[3],
         weightS1: 0.05,
         weightS2: 0.05,
-        weightS3: 0.90
+        weightS3: 0.90,
+        tonPerEuro: 0.701
     },
     {
         industry: industry[4],
         weightS1: 0.40,
         weightS2: 0.25,
-        weightS3: 0.35
+        weightS3: 0.35,
+        tonPerEuro: 0.631
     },
     {
         industry: industry[5],
         weightS1: 0.20,
         weightS2: 0.10,
-        weightS3: 0.70
+        weightS3: 0.70,
+        tonPerEuro: 1.292
     },
     {
         industry: industry[6],
         weightS1: 0.10,
         weightS2: 0.15,
-        weightS3: 0.75
+        weightS3: 0.75,
+        tonPerEuro: 0.825
     },
     {
         industry: industry[7],
         weightS1: 0.50,
         weightS2: 0.50,
-        weightS3: 0.90
+        weightS3: 0.90,
+        tonPerEuro: 2.348
     },
     {
         industry: industry[8],
         weightS1: 0.35,
         weightS2: 0.05,
-        weightS3: 0.60
+        weightS3: 0.60,
+        tonPerEuro: 0.116
     },
     {
         industry: industry[9],
         weightS1: 0.20,
         weightS2: 0.10,
-        weightS3: 0.70
+        weightS3: 0.70,
+        tonPerEuro: 1.032
     }
 ]
 
