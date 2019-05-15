53d5dd67ca29879dd850ee62d756ae76c1cd7d48
maugares <mauro.garciaesteban@gmail.com>
2019-05-15 16:20:21 +0200

    add to sampleCompany the objects marketInfo, taxInfo and taxScope

--- src/lib/sampleCompany.js
+++ src/lib/sampleCompany.js
@@ -1,6 +1,22 @@
-export const sampleCompany = {
+export const companyInfo = {
     industry: 'Food industry',
     turnover: 20000000000.00,
     turnoverGrowth: 1.00,
     profit: 0.10
+}
+
+export const marketInfo = {
+    elasticity: -0.4,
+    taxToCustomer: 1.00
+}
+
+export const taxInfo = {
+    euroPerTon: 50.0,
+    taxGrowth: 5.5
+}
+
+export const taxScope= {
+    scope1: true,
+    scope2: true,
+    scope3: true
 }
