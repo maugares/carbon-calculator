88b3dac1299df51e32099b3e3d637974cfef9c2b
BenSchuddeboom <Benzel@live.nl>
2019-05-15 12:41:37 +0200

    add placeholder landing page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <h1>Test</h1>
+            <h1 id="placeholder">Coming Soon!</h1>
         </div>
     );
 }
