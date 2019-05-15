cff27905ea5557ab7cf5c1d98eac3a4f2ae3a1a6
maugares <mauro.garciaesteban@gmail.com>
2019-05-15 11:58:54 +0200

    move taxScope into taxInfo

--- src/formulas/calculateTax/taxInfo.js
+++ src/formulas/calculateTax/taxInfo.js
@@ -2,3 +2,15 @@ export const taxInfo = {
     euroPerTon: 1,
     taxGrowth: 1
 }
+
+export const taxScope= {
+    scope1: {
+        taxable: true
+    },
+    scope2: {
+        taxable: true
+    },
+    scope3: {
+        taxable: true
+    }
+}
