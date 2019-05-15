4fcaf7bbe35693f2fa77c3edea7a8de51066e21a
maugares <mauro.garciaesteban@gmail.com>
2019-05-15 11:59:18 +0200

    create reductionTarget.js

--- src/formulas/calculateTax/reductionTarget.js
+++ src/formulas/calculateTax/reductionTarget.js
@@ -1,5 +1,13 @@
-export const reductionTarget = {
-    reductionS1: 1,
-    reductionS2: 2,
-    reductionS3: 3
+const reductionInfo = {
+    scope1: { reduction: 1 },
+    scope2: { reduction: 2 },
+    scope3: { reduction: 3 }
 }
+
+export const reductionTarget = {
+    reductionS1: reductionInfo.scope1.reduction,
+    reductionS2: reductionInfo.scope2.reduction,
+    reductionS3: reductionInfo.scope3.reduction
+}
+
+console.log(reductionTarget)
