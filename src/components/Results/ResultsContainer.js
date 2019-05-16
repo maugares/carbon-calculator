7af5a61cf25e1b384a169d7a715f25aa1a370086
BenSchuddeboom <Benzel@live.nl>
2019-05-16 17:24:45 +0200

    setup route to component

---
+++ src/components/Results/ResultsContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import PageHeader from '../PageHeader'
+
+export default class ResultsContainer extends Component {
+    render() {
+        return (
+            <div className="container">
+                <PageHeader />
+            </div>
+        )
+    }
+}
