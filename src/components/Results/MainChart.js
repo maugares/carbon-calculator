6a8961ee50923125a8982512142aef240dd49392
maugares <mauro.garciaesteban@gmail.com>
2019-05-17 16:41:44 +0200

    change var names for profit and cumulative to nameProfit and nameCumulative

--- src/components/Results/MainChart.js
+++ src/components/Results/MainChart.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import {Line} from 'react-chartjs-2'
-import {optionsEuro} from './chartData'
-import {calculateProfitWithoutTaxes, calculateProfitWithTaxes} from '../../formulas/calculateProfit/calculateProfit'
+import { Line } from 'react-chartjs-2'
+import { optionsEuro } from './chartData'
+import { dataGraphProfitNT } from '../../formulas/calculateProfit/calculateProfit'
 
 
 export default class MainChart extends Component {
@@ -9,33 +9,27 @@ export default class MainChart extends Component {
         return (
             <div className='profit-chart'>
                 <h3>Profit Per Year</h3>
-                <Line 
-                    options={optionsEuro} 
+                <Line
+                    options={optionsEuro}
                     data={
                         {
-                            labels: [1,2,3,4,5],
+                            labels: [1, 2, 3, 4, 5],
                             datasets:
-                            [
-                                {
-                                    label: "Profit after tax",
-                                    data: [
-                                        1200000*this.props.taxInfo.euroPerTon,
-                                        1300000*this.props.taxInfo.euroPerTon,
-                                        1400000*this.props.taxInfo.euroPerTon,
-                                        1500000*this.props.taxInfo.euroPerTon,
-                                        1600000*this.props.taxInfo.euroPerTon
-                                    ],
-                                    backgroundColor: "rgba(101, 188, 162, 0.3)",
-                                    pointBackgroundColor:  "rgba(101, 188, 162, 1)",
-                                },
-                                {
-                                    label: "Profit without tax",
-                                    data: Object.values(calculateProfitWithoutTaxes(this.props.companyData, 5)).map(el => el.profitWithoutTax),
-                                    backgroundColor: "rgba(69, 168, 72, 0.3)",
-                                    pointBackgroundColor:  "rgba(69, 168, 72, 1)",
-                                    fill: true,
-                                }
-                            ]
+                                [
+                                    {
+                                        label: "Profit after tax",
+                                        data: dataGraphProfitNT(this.props.companyData, 5, "profit", "cumulative", true),
+                                        backgroundColor: "rgba(101, 188, 162, 0.3)",
+                                        pointBackgroundColor: "rgba(101, 188, 162, 1)",
+                                    }
+                                    // {
+                                    //     label: "Profit without tax",
+                                    //     data: Object.values(calculateProfitWithoutTaxes(this.props.companyData, 5)).map(el => el.profitWithoutTax),
+                                    //     backgroundColor: "rgba(69, 168, 72, 0.3)",
+                                    //     pointBackgroundColor: "rgba(69, 168, 72, 1)",
+                                    //     fill: true,
+                                    // }
+                                ]
                         }
                     }
                 />
