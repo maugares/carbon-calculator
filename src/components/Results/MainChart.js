a5e762d49bc1459d64a9260fb84165a78f734448
BenSchuddeboom <Benzel@live.nl>
2019-05-17 13:25:55 +0200

    rt graph, first version

--- src/components/Results/MainChart.js
+++ src/components/Results/MainChart.js
@@ -6,35 +6,58 @@ import {calculateProfitWithoutTaxes, calculateProfitWithTaxes} from '../../formu
 
 class MainChart extends Component {
     state = {
-        sliderInput: 0,
         options,
         data: initial,
+
     }
 
     componentWillMount() {
         const companyData = this.props.pageOneInput
+
         companyData.turnoverGrowth /= 100
         companyData.profit /= 100
         companyData.taxToConsumer /= 100
 
-        const emissionData = this.props.pageTwoInput
-
-        const profitWithoutTaxes = Object.values(calculateProfitWithoutTaxes(companyData, 5))
-        const profitWithTaxes = calculateProfitWithTaxes(companyData, 5) 
-
-
-
-        initial.datasets[1].data = profitWithoutTaxes.map(el => el.profitWithoutTax)
+        const profitWithoutTaxes = Object.values(calculateProfitWithoutTaxes(companyData, 5)).map(el => el.profitWithoutTax)
         
-        console.log(this.state.data)
-        // initial.something = calculation output
+        this.setState({
+            profitWithoutTaxes
+        })
     }
 
     render() {
         if(!this.state.data) return 'loading...'
+        console.log(this.props.taxInfo.euroPerTon)
         return (
             <div className='profit-chart'>
-                <Line options={this.state.options} data={this.state.data} />
+                <Line options={this.state.options} data={
+                    {
+                        labels: [1,2,3,4,5],
+                        datasets:
+                            [
+                                {
+                                    label: "Profit after tax",
+                                    data: [
+                                        1200000*this.props.taxInfo.euroPerTon,
+                                        1300000*this.props.taxInfo.euroPerTon,
+                                        1400000*this.props.taxInfo.euroPerTon,
+                                        1500000*this.props.taxInfo.euroPerTon,
+                                        1600000*this.props.taxInfo.euroPerTon
+                                    ],
+                                    backgroundColor: "rgba(101, 188, 162, 0.3)",
+                                    pointBackgroundColor:  "rgba(101, 188, 162, 1)",
+                                },
+                                {
+                                    label: "Profit without tax",
+                                    data: Object.values(calculateProfitWithoutTaxes(this.props.pageOneInput, 5)).map(el => el.profitWithoutTax),
+                                    backgroundColor: "rgba(69, 168, 72, 0.3)",
+                                    pointBackgroundColor:  "rgba(69, 168, 72, 1)",
+                                    fill: true,
+                                }
+                            ]
+                    }
+                }
+                />
             </div>
         )
     }
