cd54e3954c36ef517b5524e54f74dc1697f77a5d
BenSchuddeboom <Benzel@live.nl>
2019-05-23 16:00:03 +0200

    add elasticity marks

--- src/components/PageOne/MarketInfoForm.js
+++ src/components/PageOne/MarketInfoForm.js
@@ -12,7 +12,7 @@ export default function MarketInfoForm(props) {
         <div className="form-container" onSubmit={props.onSubmit}>
             <h2>Market Information</h2>
             <Form>
-                <Form.Item style={{width: '45%'}} wrapperCol={{ sm: 24 }}>
+                <Form.Item style={{width: '100%'}} wrapperCol={{ sm: 12 }}>
                     <TextWithTooltip topic='elasticity'/>
                     <Slider
                         style={{width: '65%'}}
@@ -21,17 +21,17 @@ export default function MarketInfoForm(props) {
                         min={-4}
                         max={0}
                         step={0.1}
-                        marks={{0: '0', '-4': '-4'}}
+                        marks={{ '0': 'Inelastic', '-4': 'Elastic' }}
                     />
                 </Form.Item>
-                <Form.Item style={{width: '45%'}} colon={false} wrapperCol={{ sm: 24 }}>
+                <Form.Item style={{width: '100%'}} colon={false} wrapperCol={{ sm: 12 }}>
                     <TextWithTooltip topic='taxToCustomer'/>
                     <Slider
                         style={{width: '65%'}}
                         value={props.values.taxToCustomer} 
                         onChange={e => props.onChange(e, 'taxToCustomer')}
                         tipFormatter={percFormatter} 
-                        marks={{0: '0%', 100: '100%'}}
+                        marks={{ 0: '0%', 100: '100%' }}
                     />
                 </Form.Item>
             </Form>
