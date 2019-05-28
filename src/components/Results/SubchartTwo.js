3df51b875c0a6936d75f600cfab15f20a278bda5
maugares <mauro.garciaesteban@gmail.com>
2019-05-28 16:44:54 +0200

    change colours in chart emissions for better contrast

--- src/components/Results/SubchartTwo.js
+++ src/components/Results/SubchartTwo.js
@@ -14,56 +14,56 @@ export default class SubchartTwo extends Component {
                     {
                         labels: [1, 2, 3, 4, 5],
                         datasets:
-                        [
-                            {
-                                label: "Scope 1",
-                                data: dataGraphTaxableEmissions(
-                                    this.props.companyData,
-                                    this.props.taxScope,
-                                    this.props.taxInfo,
-                                    this.props.emissionData,
-                                    5,
-                                    "scope1",
-                                    "scope1Cumulative",
-                                    this.props.cumulative
-                                ),
-                                backgroundColor: "rgba(101, 188, 162, 0.3)",
-                                pointBackgroundColor: "rgba(101, 188, 162, 1)",
-                                borderColor: "rgba(101, 188, 162, 0.8)"
-                            },
-                            {
-                                label: "Scope 2",
-                                data: dataGraphTaxableEmissions(
-                                    this.props.companyData,
-                                    this.props.taxScope,
-                                    this.props.taxInfo,
-                                    this.props.emissionData,
-                                    5,
-                                    "scope2",
-                                    "scope2Cumulative",
-                                    this.props.cumulative
-                                ),
-                                backgroundColor: "rgba(69, 168, 72, 0.3)",
-                                pointBackgroundColor: "rgba(69, 168, 72, 1)",
-                                borderColor: "rgba(69, 168, 72, 0.8)"
-                            },
-                            {
-                                label: "Scope 3",
-                                data: dataGraphTaxableEmissions(
-                                    this.props.companyData,
-                                    this.props.taxScope,
-                                    this.props.taxInfo,
-                                    this.props.emissionData,
-                                    5,
-                                    "scope3",
-                                    "scope3Cumulative",
-                                    this.props.cumulative
-                                ),
-                                backgroundColor: "rgba(60, 116, 151, 0.3)",
-                                pointBackgroundColor: "rgba(60, 116, 151, 0.8)",
-                                borderColor: "rgba(60, 116, 151, 0.8)"
-                            },
-                        ]
+                            [
+                                {
+                                    label: "Scope 1",
+                                    data: dataGraphTaxableEmissions(
+                                        this.props.companyData,
+                                        this.props.taxScope,
+                                        this.props.taxInfo,
+                                        this.props.emissionData,
+                                        5,
+                                        "scope1",
+                                        "scope1Cumulative",
+                                        this.props.cumulative
+                                    ),
+                                    backgroundColor: "rgba(60, 116, 151, 0.3)",
+                                    pointBackgroundColor: "rgba(60, 116, 151, 0.8)",
+                                    borderColor: "rgba(60, 116, 151, 0.8)"
+                                },
+                                {
+                                    label: "Scope 2",
+                                    data: dataGraphTaxableEmissions(
+                                        this.props.companyData,
+                                        this.props.taxScope,
+                                        this.props.taxInfo,
+                                        this.props.emissionData,
+                                        5,
+                                        "scope2",
+                                        "scope2Cumulative",
+                                        this.props.cumulative
+                                    ),
+                                    backgroundColor: "rgba(69, 168, 72, 0.3)",
+                                    pointBackgroundColor: "rgba(69, 168, 72, 1)",
+                                    borderColor: "rgba(69, 168, 72, 0.8)"
+                                },
+                                {
+                                    label: "Scope 3",
+                                    data: dataGraphTaxableEmissions(
+                                        this.props.companyData,
+                                        this.props.taxScope,
+                                        this.props.taxInfo,
+                                        this.props.emissionData,
+                                        5,
+                                        "scope3",
+                                        "scope3Cumulative",
+                                        this.props.cumulative
+                                    ),
+                                    backgroundColor: "rgba(101, 188, 162, 0.3)",
+                                    pointBackgroundColor: "rgba(101, 188, 162, 1)",
+                                    borderColor: "rgba(101, 188, 162, 0.8)"
+                                },
+                            ]
                     }
                 } />
             </div>
