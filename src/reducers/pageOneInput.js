10f4b8c7d2fe8f59be074b0a8e68e218303479ad
BenSchuddeboom <benzel@live.nl>
2019-05-28 13:07:39 +0200

    remove 'klimaatakkoord' from favicon

--- src/reducers/pageOneInput.js
+++ src/reducers/pageOneInput.js
@@ -6,13 +6,13 @@ const initialState = JSON.parse(sessionStorage.getItem('companyInfo')) || {
     turnoverGrowth: 10
 }
 
-export default (state = initialState, action = {}) => {
+export default ( state = initialState, action = {} ) => {
     switch(action.type) {
     case SUBMIT_INPUT_ONE:
-        sessionStorage.setItem('companyInfo', JSON.stringify({...state, ...action.payload}))
-        return {...state, ...action.payload}
+        sessionStorage.setItem( 'companyInfo', JSON.stringify({...state, ...action.payload} ))
+        return { ...state, ...action.payload }
     case UPDATE_INPUT:
-        return {...state, ...action.payload}
+        return { ...state, ...action.payload }
     default:
         return state
     }
