a3b12ac5cfff11e7501f375c68f4bfedeeda0e37
BenSchuddeboom <Benzel@live.nl>
2019-06-02 12:27:14 +0200

    improved slider contrast

--- config-overrides.js
+++ config-overrides.js
@@ -11,7 +11,13 @@ module.exports = override(
         modifyVars: { // Modify theme LESS theme below
             '@primary-color': '#45A848',
             '@heading-color': '#40809C',
-            '@font-family': "'-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', 'sans-serif'"
+            '@font-family': "'-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', 'sans-serif'",
+            '@slider-rail-background-color': '#d1d1d1',
+            '@slider-rail-background-color-hover': '#c1c1c1',
+            '@slider-track-background-color': '@primary-5',
+            '@slider-track-background-color-hover': '@primary-6',
+            '@slider-handle-color': '@primary-5',
+            '@slider-handle-color-hover': '@primary-6'
         },
     }),
 );
